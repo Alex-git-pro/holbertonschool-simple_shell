b40fa2295059ab0170f08c449f1c96624edbf852
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-12-19 17:38:21 +0700

    Added ocd.c

---
+++ ocd.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * ocd - Modifies the current working directory of the shell
+ * @args: Array containing the command and its arguments
+ *
+ * Return: -1 in all cases
+ */
+
+int ocd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "cd: no destination directory specified\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("cd: unable to switch to specified directory");
+		}
+	}
+	return (-1);
+}
