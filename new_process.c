3a5e1029c0aede8f9165d0e798e7217bd3d1a14f
Entwoane <antoine.musci@gmail.com>
2025-01-07 11:21:35 +0100

    Function to create a new process, containing two other functions. One to get the env variable and the other that attempts to execute a command in each PATH dir

--- new_process.c
+++ new_process.c
@@ -2,24 +2,26 @@
 /**
  * get_env - Function to get the env variable
  * @name: name of the var
- * Return: NULL
+ * Return: NULL if not found, pointer to value if found
  */
 char *get_env(const char *name)
 {
-	char **env; /* */
-	char *entry;
-	size_t len;
+	char **env; /*Pointer to iterate though env variables*/
+	char *entry;/*Pointer to hold each env var entry*/
+	size_t len;/*Length of the var name*/
 
 	len = strlen(name);
-	for (env = environ; *env != NULL; env++)
+	for (env = environ; *env != NULL; env++)/*Iterate through all env var*/
 	{
 		entry = *env;
 		if (strncmp(entry, name, len) == 0 && entry[len] == '=')
+		/*Check if current entry matches the requested name*/
 		{
 			return (entry + len + 1);
+			/*Return pointer to the value part of the environment variable*/
 		}
 	}
-	return (NULL);
+	return (NULL); /*Return NULL if the variable is not found*/
 }
 /**
  * try_exec - Function to attempt to exec a command in each PATH dir
@@ -33,7 +35,7 @@ int try_exec(char **args)
 	char *dir;
 	char full_path[1024];
 
-	path = get_env("PATH");
+	path = get_env("PATH");/*Retrieve the PATH env variable*/
 	if (path != NULL)
 	{
 		path_copy = strdup(path);
@@ -42,18 +44,21 @@ int try_exec(char **args)
 		perror("shell");
 			exit(EXIT_FAILURE);
 		}
+		/*Tokenize the PATH string into individuals directories*/
 		dir = strtok(path_copy, ":");
 		while (dir != NULL)
 		{
+			/*Construct the full path of the command*/
 			strcpy(full_path, dir);
 			strcat(full_path, "/");
 			strcat(full_path, args[0]);
-			execve(full_path, args, environ);
-			dir = strtok(NULL, ":");
+			execve(full_path, args, environ);/*Attempt to execute the command*/
+			/*If execve succeeds, doesn't return. If it return if failed*/
+			dir = strtok(NULL, ":");/*Move to the next directory PATH*/
 		}
 	}
-		free(path_copy);
-		return (-1);
+		free(path_copy);/*Free the memory allocated for the PATH copy*/
+		return (-1);/*If we reach here, command was not found in any PATH dir*/
 }
 /**
  * new_process - Function to create a new process
@@ -65,25 +70,26 @@ int new_process(char **args)
 	pid_t pid;
 	int status;
 
-	pid = fork();
+	pid = fork();/*Fork a nex process*/
 	if (pid == 0)
 	{
+		/*Child process*/
 		if (try_exec(args) == -1)
 		{
+			/*If execution fails, print error and exit*/
 			perror("shell");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else if (pid < 0)
-	{
+	{	/*Fork failed*/
 		perror("shell");
 	}
 	else
-	{
+	{	/*Parent process*/
 		do {
-			waitpid(pid, &status, WUNTRACED);
+			waitpid(pid, &status, WUNTRACED);/*Wait for the child process to change state*/
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 return (1);
 }
-
