fa33e36ce2e85e4076df8d02fac3b2ddbfefcb20
Entwoane <antoine.musci@gmail.com>
2025-01-07 11:02:55 +0100

    Adding comments

--- read_line.c
+++ read_line.c
@@ -6,22 +6,23 @@
  */
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t bufsize = 0;
+	char *line = NULL; /*Pointer to store the line and initialize it to NULL*/
+	size_t bufsize = 0;/* Variable to store the size og the allocated buffer*/
 
-	if (getline(&line, &bufsize, stdin) == -1)
+	/*Read line from stdin with getline*/
+	if (getline(&line, &bufsize, stdin) == -1)/*getline() allocates mem for line*/
 	{
-		if (feof(stdin))
+		if (feof(stdin))/*Check if we've reached the EOF*/
 		{
-			free(line);
+			free(line);/*If EOF, free the allocated memory and exit*/
 			exit(EXIT_SUCCESS);
 		}
 		else
-		{
+		{	/*If there's an error, free allocated mem and exit*/
 			free(line);
 			perror("error while reading the line from stdin");
-			exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE); /*Exit with failure status*/
 		}
 	}
-	return (line);
+	return (line);/*Return pointer to the read line if getline() was successful*/
 }
