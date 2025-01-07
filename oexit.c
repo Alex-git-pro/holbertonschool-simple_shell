e1e1e6f23ccb34e274b33aeba1e60d1f1f4edcdd
Entwoane <antoine.musci@gmail.com>
2025-01-07 11:02:43 +0100

    Adding comments

--- oexit.c
+++ oexit.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * oexit - Function that exit the program
- * @args: empty args
- * Return: 0
+ * oexit - Function that exits the program
+ * @args: array of arguments passed to the function
+ * Return: Exit status code
  */
 int oexit(char **args)
 {
-	if (args[1])
+	if (args[1]) /*Check if there's a second argument*/
 	{
 		return (atoi(args[1]));
+		/*If yes convert it to an int and return it as the exit status*/
 	}
 	else
 	{
-		return (0);
+		return (0); /*If not, return 0 as the default exit status*/
 	}
 }
