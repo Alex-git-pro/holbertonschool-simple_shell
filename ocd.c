0c0fe59b251c92ab46fa8191bd15392f3f87e4ff
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-07 17:30:36 +0700

    Adding ocd.c ohelp.c

--- ocd.c
+++ ocd.c
@@ -9,16 +9,21 @@
 
 int ocd(char **args)
 {
+	/* Check if a directory argument is provided */
 	if (args[1] == NULL)
 	{
+		/* Print error message if no directory is specified */
 		fprintf(stderr, "cd: no destination directory specified\n");
 	}
 	else
 	{
+		/* Attempt to change directory */
 		if (chdir(args[1]) != 0)
 		{
+			/* Print error message if directory change fails */
 			perror("cd: unable to switch to specified directory");
 		}
 	}
+	/* Return 1 to continue shell execution */
 	return (1);
 }
--- ohelp.c
+++ ohelp.c
@@ -9,6 +9,7 @@
 
 int ohelp(char **args)
 {
+	/* Array of built-in command names */
 	char *builtin_func_list[] =
 
 	{
@@ -19,15 +20,21 @@ int ohelp(char **args)
 	};
 
 	unsigned long int i = 0;
+	/* Avoid unused parameter warning */
 	(void)(**args);
 
+	/* Print help header */
 	printf("\n---help simple_shell---\n");
+	/* Print general usage instruction */
 	printf("Enter your command followed by arguments\n");
+	/* Print built-in commands header */
 	printf("Built-in commands:\n");
+	/* Loop through and print each built-in command */
 	for (i = 0; i < sizeof(builtin_func_list) / sizeof(char *); i++)
 	{
 		printf("  -> %s\n", builtin_func_list[i]);
 	}
+	/* Print additional help information */
 	printf("Use the man command to get details on other programs.\n\n");
 	return (-1);
 }
