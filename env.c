cd19a0ba922c4dc903f70cacfa2b4981512aa7fe
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-10 16:54:33 +0700

    comment env.c

--- env.c
+++ env.c
@@ -1,16 +1,17 @@
 #include "shell.h"
 
 /**
- * print_env - Prints all environment variables.
- *
+ * print_env - Prints all the environment variables.
  */
 void print_env(void)
 {
-	char **env = environ;
+	char **env = environ; /* Pointer to the global 'environ' variable. */
 
+	/*Loop through all the variables until a NULL pointer is encountered.*/
 	while (*env)
 	{
+		 /*Print the current environment variable followed by a newline.*/
 		printf("%s\n", *env);
-		env++;
+		env++; /* Move to the next environment variable in the array. */
 	}
 }
